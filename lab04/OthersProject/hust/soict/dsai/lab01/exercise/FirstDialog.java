dace93ed2cd1da0e48a08aa81a2c577d06072cea
NMH280203 <121481127+NMH280203@users.noreply.github.com>
2023-12-14 16:24:09 +0700

    Delete lab04 directory

--- lab04/OthersProject/hust/soict/dsai/lab01/exercise/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-// Nguyen Manh Hieu 20215050
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Nguyen Manh Hieu - 5050-Hello World! How are you?");
-        System.exit(0);
-    }
-}
